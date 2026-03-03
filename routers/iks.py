a2bd4de25de430ba636548f0e881c2b920a1ce44
windyrahayu45 <windyrahayuuu@gmail.com>
2026-03-03 23:27:01 +0700

    Refactor iks.py and iks_service.py to remove commented code and update model path handling

--- routers/iks.py
+++ routers/iks.py
@@ -1,21 +1,13 @@
-# from fastapi import APIRouter
-# from schemas.iks_schema import IKSInput
-# from services.iks_service import predict_iks
-
-# router = APIRouter()
-
-# @router.post("/predict")
-# def predict(data: IKSInput):
-#     score = predict_iks(data)
-
-#     return {
-#         "iks_score": score
-#     }
-
 from fastapi import APIRouter
+from schemas.iks_schema import IKSInput
+from services.iks_service import predict_iks
 
 router = APIRouter()
 
-@router.get("/")
-def test_iks():
-    return {"iks": "router working"}
+@router.post("/predict")
+def predict(data: IKSInput):
+    score = predict_iks(data)
+
+    return {
+        "iks_score": score
+    }
--- services/iks_service.py
+++ services/iks_service.py
@@ -1,13 +1,42 @@
+# import joblib
+# import numpy as np
+# from pathlib import Path
+
+# _model = None
+
+# def get_model():
+#     global _model
+#     if _model is None:
+#         model_path = Path(__file__).resolve().parent.parent / "models" / "iks_model_v1.pkl"
+#         _model = joblib.load(model_path)
+#     return _model
+
+# def predict_iks(data):
+#     model = get_model()
+
+#     features = np.array([[
+#         data.economic_score,
+#         data.education_numeric,
+#         data.employment_flag,
+#         data.age_bucket,
+#         data.aid_count
+#     ]])
+
+#     prediction = model.predict(features)[0]
+#     return round(float(prediction), 2)
 import joblib
 import numpy as np
-from pathlib import Path
+import os
 
 _model = None
 
 def get_model():
     global _model
     if _model is None:
-        model_path = Path(__file__).resolve().parent.parent / "models" / "iks_model_v1.pkl"
+        base_dir = os.getcwd()  # ini penting
+        model_path = os.path.join(base_dir, "models", "iks_model_v1.pkl")
+        print("MODEL PATH:", model_path)
+        print("FILE EXISTS:", os.path.exists(model_path))
         _model = joblib.load(model_path)
     return _model
 
