9098172e1e88adc23c1e7bf7386a1ab357cc2709
windyrahayu45 <windyrahayuuu@gmail.com>
2026-03-04 08:40:37 +0700

    Refactor iks.py to remove commented-out code and update route handling

--- routers/iks.py
+++ routers/iks.py
@@ -1,13 +1,24 @@
+# from fastapi import APIRouter
+# from schemas.iks_schema import IKSInput
+# from services.iks_service import predict_iks
+
+# router = APIRouter()
+
+# @router.post("/predict")
+# def predict(data: IKSInput):
+#     score = predict_iks(data)
+
+#     return {
+#         "iks_score": score
+#     }
+
 from fastapi import APIRouter
-from schemas.iks_schema import IKSInput
-from services.iks_service import predict_iks
+from services import iks_service
 
 router = APIRouter()
 
-@router.post("/predict")
-def predict(data: IKSInput):
-    score = predict_iks(data)
+print("IKS ROUTER IMPORTED")
 
-    return {
-        "iks_score": score
-    }
+@router.get("/")
+def test_iks():
+    return {"iks": "router working"}
