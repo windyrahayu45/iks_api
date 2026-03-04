eddc6c0b17ff5b08bc7b9c5764c120f2c752cd82
windyrahayu45 <windyrahayuuu@gmail.com>
2026-03-04 08:56:46 +0700

    Refactor iks.py to remove commented-out code and implement prediction endpoint

--- routers/iks.py
+++ routers/iks.py
@@ -1,30 +1,14 @@
-# from fastapi import APIRouter
-# from schemas.iks_schema import IKSInput
-# from services.iks_service import predict_iks
-
-# router = APIRouter()
-
-# @router.post("/predict")
-# def predict(data: IKSInput):
-#     score = predict_iks(data)
-
-#     return {
-#         "iks_score": score
-#     }
-
 from fastapi import APIRouter
-from services.iks_service import get_model
-import numpy as np
+from schemas.iks_schema import IKSInput
+from services.iks_service import predict_iks
 
 router = APIRouter()
 
-@router.get("/")
-def test_iks():
-    model = get_model()
+@router.post("/predict")
+def predict(data: IKSInput):
+    score = predict_iks(data)
 
-    # dummy features sesuai jumlah kolom model
-    features = np.array([[0, 0, 0, 0, 0]])
+    return {
+        "iks_score": score
+    }
 
-    result = model.predict(features)
-
-    return {"result": result.tolist()}
